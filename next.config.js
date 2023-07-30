acb3ea65413dd8a3dbdf944bae9d79a2e8fc5574
Alexander Claes <alexander.claes10@gmail.com>
2023-07-30 17:31:26 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         port: "3000",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "api.tracky.be",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
