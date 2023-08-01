291800351af3ed83a6ef175e2737dc7e8e4e1d8b
Alexander Claes <alexander.claes10@gmail.com>
2023-08-01 21:43:37 +0200

    Refactor endpoints file

--- api/endpoints.ts
+++ api/endpoints.ts
@@ -1,33 +1,49 @@
-const h = (pathname: string) => `${process.env.NEXT_PUBLIC_API}${pathname}`;
+const apiUrl = (pathname: string) =>
+  `${process.env.NEXT_PUBLIC_API}${pathname}`;
 
-export default {
-  auth: h("/auth"),
-  authSignIn: h("/auth/sign_in"),
-  authValidateToken: h("/auth/validate_token"),
-  createFolder: h("/folders"),
-  createProject: h("/projects"),
-  createTask: h("/tasks"),
-  createTimeEntry: h("/time_entries"),
-  destroyFolder: (folderId: number) => h(`/folders/${folderId}`),
-  destroyProject: (projectId: number) => h(`/projects/${projectId}`),
-  destroyTask: (taskId: number) => h(`/tasks/${taskId}`),
-  destroyTimeEntry: (timeEntryId: number) => h(`/time_entries/${timeEntryId}`),
-  getRunningTimers: h("/misc/running_timers"),
-  getTimesheetsPerDay: h("/misc/timesheets_per_day"),
-  getTimesheetsPerWeek: h("/misc/timesheets_per_week"),
-  getTimeEntry: (timeEntryId: number) => h(`/time_entries/${timeEntryId}`),
-  getTask: (taskId: number) => h(`/tasks/${taskId}`),
-  getTasks: h("/tasks"),
-  getTimeEntries: h("/time_entries"),
-  getFolder: (folderId: number) => h(`/folders/${folderId}`),
-  getFoldersFromProject: (projectId: number) =>
-    h(`/projects/${projectId}/folders`),
-  getProject: (projectId: number) => h(`/projects/${projectId}`),
-  getProjects: h("/projects"),
-  getTasksFromFolder: (folderId: number) => h(`/folders/${folderId}/tasks`),
+const apiRoutes = {
+  // Auth
+  auth: apiUrl("/auth"),
+  authSignIn: apiUrl("/auth/sign_in"),
+  authValidateToken: apiUrl("/auth/validate_token"),
+
+  // Folders
+  createFolder: apiUrl("/folders"),
+  destroyFolder: (folderId: number) => apiUrl(`/folders/${folderId}`),
+
+  // Projects
+  createProject: apiUrl("/projects"),
+  destroyProject: (projectId: number) => apiUrl(`/projects/${projectId}`),
+
+  // Tasks
+  createTask: apiUrl("/tasks"),
+  destroyTask: (taskId: number) => apiUrl(`/tasks/${taskId}`),
+  getTask: (taskId: number) => apiUrl(`/tasks/${taskId}`),
+  getTasks: apiUrl("/tasks"),
+  getTasksFromFolder: (folderId: number) =>
+    apiUrl(`/folders/${folderId}/tasks`),
+
+  // Time Entries
+  createTimeEntry: apiUrl("/time_entries"),
+  destroyTimeEntry: (timeEntryId: number) =>
+    apiUrl(`/time_entries/${timeEntryId}`),
+  getTimeEntry: (timeEntryId: number) => apiUrl(`/time_entries/${timeEntryId}`),
+  getTimeEntries: apiUrl("/time_entries"),
   getTimeEntriesFromTask: (taskId: number) =>
-    h(`/tasks/${taskId}/time_entries`),
+    apiUrl(`/tasks/${taskId}/time_entries`),
 
-  //misc
-  stopTimeEntries: h("/misc/stop/all"),
+  // Misc
+  getRunningTimers: apiUrl("/misc/running_timers"),
+  getTimesheetsPerDay: apiUrl("/misc/timesheets_per_day"),
+  getTimesheetsPerWeek: apiUrl("/misc/timesheets_per_week"),
+  stopTimeEntries: apiUrl("/misc/stop/all"),
+
+  // Other routes
+  getFolder: (folderId: number) => apiUrl(`/folders/${folderId}`),
+  getFoldersFromProject: (projectId: number) =>
+    apiUrl(`/projects/${projectId}/folders`),
+  getProject: (projectId: number) => apiUrl(`/projects/${projectId}`),
+  getProjects: apiUrl("/projects"),
 };
+
+export default apiRoutes;
