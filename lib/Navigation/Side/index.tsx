6605c3b0980b6846225c09884978cedcf87b1219
Alexander Claes <alexander.claes10@gmail.com>
2023-08-04 23:11:52 +0200

    Add mobile overlay for nav

--- lib/Navigation/Side/index.tsx
+++ lib/Navigation/Side/index.tsx
@@ -30,44 +30,51 @@ export default function SideNavigation(props: SideNavigationProps) {
   };
 
   return (
-    <div
-      className={`${styles.root} ${
-        shouldToggleMobileMenu ? styles.moveOut : ""
-      } ${isMobileMenuOpen ? styles.moveIn : ""}`}
-    >
-      <div className={styles.secondRoot} />
-      <div className={styles.thirdRoot} />
-      <div className={styles.logoWrapper}>
-        <Link href="/" className={styles.logo} onClick={onLinkClick}>
-          <Logo small white noLink />
-        </Link>
-      </div>
-      <div className={styles.inner}>
-        <SideNavigationRunningTimeEntries />
-        <SideNavigationLink
-          href="/app/timer"
-          title="Get started"
-          sub="Start inserting time entries for your tasks"
-        />
-        <SideNavigationLink
-          href="/app/dashboard"
-          title="Dashboard"
-          sub="Manage your projects"
-        />
-        {/* <SideNavigationLink
+    <>
+      <div
+        className={`${styles.mobileOverlay} ${
+          shouldToggleMobileMenu ? styles.opacityOverlay : ""
+        } ${isMobileMenuOpen ? styles.opacityOverlay : ""}`}
+      />
+      <div
+        className={`${styles.root} ${
+          shouldToggleMobileMenu ? styles.moveOut : ""
+        } ${isMobileMenuOpen ? styles.moveIn : ""}`}
+      >
+        <div className={styles.secondRoot} />
+        <div className={styles.thirdRoot} />
+        <div className={styles.logoWrapper}>
+          <Link href="/" className={styles.logo} onClick={onLinkClick}>
+            <Logo small white noLink />
+          </Link>
+        </div>
+        <div className={styles.inner}>
+          <SideNavigationRunningTimeEntries />
+          <SideNavigationLink
+            href="/app/timer"
+            title="Get started"
+            sub="Start inserting time entries for your tasks"
+          />
+          <SideNavigationLink
+            href="/app/dashboard"
+            title="Dashboard"
+            sub="Manage your projects"
+          />
+          {/* <SideNavigationLink
           href="/app/timeline"
           title="Calendar"
           sub="Use a timeline for inserting time entries"
           wip
         /> */}
-        <SideNavigationLink
-          href="/app/timesheets"
-          title="Timesheets generator"
-          sub="Generate timesheets for a specific date range"
-          wip
-        />
-        <SideNavigationProjects />
+          <SideNavigationLink
+            href="/app/timesheets"
+            title="Timesheets generator"
+            sub="Generate timesheets for a specific date range"
+            wip
+          />
+          <SideNavigationProjects />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
